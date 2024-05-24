2933fda981d329dc86f8f8b97ebe1591e3b06284
Arief Budiman <mariefbudiman12@gmail.com>
2024-05-24 11:11:25 +0700

    checkpoint#4

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,11 @@
-/** @type {import('tailwindcss').Config} */
+import type { Config } from 'tailwindcss';
 
-module.exports = {
+const config: Config = {
   content: [
     './node_modules/flowbite-react/**/*.js',
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/**/*.{ts,tsx}',
   ],
   theme: {
     extend: {
@@ -18,3 +17,4 @@ module.exports = {
   },
   plugins: [require('flowbite/plugin')],
 };
+export default config;
