b1443beedcaab1038e4d1dfba4701d5319ffc57d
Arief Budiman <mariefbudiman12@gmail.com>
2024-05-28 12:05:07 +0700

    modify canteens menu

--- src/components/container/menus-canteen-container.tsx
+++ src/components/container/menus-canteen-container.tsx
@@ -1,10 +1,20 @@
 import { TMenus } from '@/libs/types';
 import React from 'react';
 import MenusCard from '../UI/menus-card';
+import { HiInformationCircle } from 'react-icons/hi';
+import { Alert, CustomFlowbiteTheme, Flowbite } from 'flowbite-react';
 
 interface MenusCanteenContainerProps {
   menus?: TMenus[];
 }
+
+const customTheme: CustomFlowbiteTheme['alert'] = {
+  base: 'flex flex-col p-4 text-sm mx-auto md:w-1/2',
+  color: {
+    failure:
+      'bg-red-200 text-red-700 transition-all duration-300 hover:bg-red-300',
+  },
+};
 
 export default function MenusCanteenContainer({
   menus,
@@ -13,7 +23,9 @@ export default function MenusCanteenContainer({
     <>
       <div className='text-slate-800'>
         {!menus?.length ? (
-          <span>All Menus will be displayed here.</span>
+          <Alert theme={customTheme} color='failure' icon={HiInformationCircle}>
+            <p className='px-3'>All Menus will be displayed here.</p>
+          </Alert>
         ) : (
           menus.map((menu) => <MenusCard menu={menu} />)
         )}
