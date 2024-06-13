b4d305634a3e8bf70ad9e30f7e09f4468da624e1
Arief Budiman <mariefbudiman12@gmail.com>
2024-06-13 23:47:44 +0700

    deleted getUserSession func

--- src/libs/utils/index.ts
+++
@@ -1,8 +0,0 @@
-import { Session } from 'next-auth';
-import getSession from '../auth/getSession';
-
-export async function getSessionToken() {
-  const session = (await getSession()) as Session;
-  const sessionToken = session.sessionToken;
-  return session ?? 'null';
-}
